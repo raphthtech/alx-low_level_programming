9a4c8ff6bc65229ca9ee31c052ae007be37d81ff
raphthtech <raphaellthomas@gmail.com>
2022-04-08 13:42:44 -0700

    task 2 done

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- * main - the entry point 
+ * main: the entry point
  *
- * Description - program the print lowercase alphabets
- * Return - 0
+ * Description: program the print lowercase alphabets
+ * Return:  0
  */
 int main(void)
 {
 	int c;
 
-	for(c = 0; c < 26; ++c)
+	for (c = 0; c < 26; ++c)
 	putchar('a' + c);
 
 	putchar ('\n');
