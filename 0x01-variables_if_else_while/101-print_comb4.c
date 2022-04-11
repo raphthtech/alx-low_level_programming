914440da4eb4a42a420cd829556a7f1070ed897e
raphthtech <raphaellthomas@gmail.com>
2022-04-11 06:10:48 -0700

    task 11 done

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,7 +21,7 @@ int main(void)
 	putchar((num2 % 10) + '0');
 	putchar((num3 % 10) + '0');
 	
-	if (num1 == 7 && num2 == 9 && num3 == 9)
+	if (num1 == 7 && num2 == 8 && num3 == 9)
 	continue;
 
 	putchar(',');
