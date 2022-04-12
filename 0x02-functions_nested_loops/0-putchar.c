4b7d2213037bece5ee942856f0f4c577c792765e
raphthtech <raphaellthomas@gmail.com>
2022-04-12 06:19:05 -0700

    task 0 done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - main block
+ *
+ * Descrtiption: print _putchar on a new line
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_purchar('a');
+	_purchar('r');
+	_putchar('\n');
+	return (0);
+}
