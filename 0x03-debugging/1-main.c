2a797123efd2d340206a78035e03906284705b6c
raphthtech <raphaellthomas@gmail.com>
2022-04-14 06:42:42 -0700

    task 1 done

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
 /**
  * main - causes an infinite loop
- *
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -12,7 +13,7 @@ int main(void)
 
 	i = 0;
 
-	/**
+	/*
 	 * while (i < 10)
 	 * {
 	 * putchar(i);
