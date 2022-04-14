0ec730e477b00ef81faf0135b86d71b0d5fcef0a
raphthtech <raphaellthomas@gmail.com>
2022-04-14 11:15:23 -0700

    task 1 done

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,12 +2,12 @@
 /**
  * _isdigit - if a character is a digit
  * @c: print int
- * Return: Always 0
+ * Return: Always 0.
  */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= 48 && c <= 57)
 	{
 	return (1);
 	}
