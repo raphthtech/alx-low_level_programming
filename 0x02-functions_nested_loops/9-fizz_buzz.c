a1c4f376c03faa7bead628a5530bd81e78fc70c7
raphthtech <raphaellthomas@gmail.com>
2022-04-15 12:40:42 -0700

    task 9 done

---
+++ 0x02-functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * main - prints fizz for in disivible by 3 and buzz for 5
+ *
+ * Return: int
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+	if (i % 3 == 0 && i % 5 != 0)
+	{
+	printf("Fizz");
+	}
+	else if (i % 3 != 0 && i % 5 == 0)
+	{
+	printf("Buzz");
+	}
+	else if (i % 3 == 0 && i % 5 == 0)
+	{
+	printf("FizzBuzz");
+	}
+	else
+	{
+	printf("%d", i);
+	}
+	if (i != 100)
+	putchar(' ');
+	else
+	putchar('\n');
+	}
+	return (0);
+}
