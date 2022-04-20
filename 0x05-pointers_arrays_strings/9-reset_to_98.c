f9c4d75c12ef7ac5dddcb82668bffd2a374cc926
raphthtech <raphaellthomas@gmail.com>
2022-04-20 08:11:22 -0700

    task 0 done

---
+++ 0x05-pointers_arrays_strings/9-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - updates a integer value to 98
+ *@n: A pointer to an integer to be updated
+ *
+ *Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains scripts on C Pointers, Arrays and Strings
