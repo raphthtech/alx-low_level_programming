c8225ceddf38253473a69d2bc713947aeeda7362
raphthtech <raphaellthomas@gmail.com>
2022-04-24 04:48:54 -0700

    task 4 done

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,6 +12,7 @@ void reverse_array(int *a, int n)
 int *start_c, *end_c, c;
 int i;
 
+
 start_c = a;
 end_c = a;
 
@@ -21,8 +22,12 @@ for (i = 0; i < n - 1; i++)
 end_c++;
 }
 
+
+
+
 for (i = 0; i < n / 2; i++)
 {
+
 
 c = *end_c;
 *end_c = *start_c;
