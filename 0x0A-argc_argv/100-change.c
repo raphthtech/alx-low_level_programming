bfc1e5974de337f8b4cea2e790d018cc7ac4f0f5
raphthtech <raphaellthomas@gmail.com>
2022-05-02 12:59:13 -0700

    task 5 done

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,36 +2,34 @@
 #include <stdlib.h>
 
 /**
- * main - function
- *@argc: length of argv
- *@argv: number of argument
- *Return: Always 0
+ * main - print the minimum number of coins to make change
+ * @argc: int
+ * @argv: string
+ * Return: int
  */
 
 int main(int argc, char *argv[])
 {
-/*Declaring variables*/
-int position, total, change, aux;
-int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
+	int i, n, s, ch = 0;
+	int c[] = {25, 10, 5, 2, 1};
 
-position = total = change = aux = 0;
-
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	s = atoi(argv[1]);
+	if (s < 1)
+		printf("0\n");
+	else
+	{
+		for (i = 0; i < 5 && s; i++)
+		{
+			n = s / c[i];
+			ch += n;
+			s -= n * c[i];
+		}
+		printf("%d\n", ch);
+	}
+	return (0);
 }
-
-total = atoi(argv[1]); /*Covert str to int*/
-
-if (total <= 0)
-{
-printf("0\n");
-return (0);
-}
-
-/*Declaring While*/
-
-while (coins[position] != '\0')
-
-{
