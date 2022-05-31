62ffda107a3b97dc108ecc7fda56a4708a1963e6
raphthtech <raphaellthomas@gmail.com>
2022-05-31 09:46:29 -0700

    task 0 done

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -1,47 +1,26 @@
-#include "hmain.h"
+#include "main.h"
 
 /**
- * _strlen - lenght of string
- * @s: char
- * Return: int
- */
-int _strlen(const char *s)
-{
-	int i;
-
-		for (i = 0; s[i] != '\0'; i++)
-		{
-			continue;
-		}
-return (i);
-}
-/**
- * binary_to_uint - convert binary to unsigned int
- * @b:string from 0 and 1 else NULL
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: A pointer to a string of 0 and 1 chars.
  *
- * Return:unsigned int
+ * Return: If b is NULL or contains chars not 0 or 1 - 0.
+ *         Otherwise - the converted number.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int k = 1;
-	unsigned int i = 0;
-	int c;
-	unsigned int len;
+	unsigned int num = 0;
+	int len = 0;
 
-	if (b == NULL)
+	if (b[len] == '\0')
 		return (0);
 
-	len = _strlen(b);
-
-	for (c = len - 1; c >= 0; c--)
+	while ((b[len] == '0') || (b[len] == '1'))
 	{
-	if (b[c] != '0' && b[c] != '1')
-		return (0);
-	if (b[c] == '1')
-	{
-		i += k;
+		num <<= 1;
+		num += b[len] - '0';
+		len++;
 	}
-	k *= 2;
-}
-	return (i);
+
+	return (num);
 }
